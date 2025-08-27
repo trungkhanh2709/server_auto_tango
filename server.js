34150f7a135150ee8754a342995b410e61f89ced
trungkhanh2709 <trungkhanh@reelsights.com>
2025-08-28 02:01:06 +0700

    fix cor

--- server.js
+++ server.js
@@ -86,7 +86,12 @@ async function typeReactInput(page, selector, text, log) {
   log(`Typed: "${text}"`);
   return true;
 }
-
+app.options("/run-tango-sse", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.sendStatus(200);
+});
 app.get("/run-tango-sse", async (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
